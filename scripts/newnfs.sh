792fcb66e2e33a6626823043c9c5f5f87b14d5f1
Eric <eubdsantos@gmail.com>
2023-02-08 10:21:36 -0300

    Fix errors

--- scripts/newnfs.sh
+++ scripts/newnfs.sh
@@ -3,15 +3,18 @@ echo "Insira o nome do volume"
 read nome;
 
 # Volume que será criado
-nfsvol=/mnt/nfsvol/$nome
+nfsvol=/mnt/nfsvol/lv01/$nome
 mkdir $nfsvol
 rede="172.0.0.0/16(rw,sync,no_root_squash,no_sub_tree_check)";
-echo $nfsvol $rede >> /etc /exports
+echo $nfsvol $rede >> /etc/exports
 exports -a 
 systemctl restart nfs-kernel-server
 echo "Volume criado"
+echo "Criação do volume finalizada"
 
+# du.sh
 echo 'echo "---"' >> du.sh
 echo 'echo "---' $nome '--"' >> du.sh
-echo "du -sh /mnt/nfsvol/lv01/"$nome >> du.sh
+echo "du -sh /mnt/nfsvol/lv01/"$nome $rede >> du.sh
+echo 'echo "---' $nome '--"' >> du.sh
 echo "Finalizada a adição no script du.sh"
