5eb7bc96466ef3955f337eab9770165c7edbf338
Eric <eubdsantos@gmail.com>
2023-02-08 14:15:27 -0300

    Fix errors and change subnet

--- scripts/newnfs.sh
+++ scripts/newnfs.sh
@@ -2,17 +2,18 @@
 echo "Insira o nome do Volume a ser criado:"
 read nome;
 
-#Volume a ser criado
+# Volume a ser criado
 nfsvol=/mnt/nfsvol/lv01/$nome
 mkdir $nfsvol
-info="172.0.0.0/16(rw,sync,no_root_squash,no_subtree_check)";
+# Linode
+info="192.168.128.0/21(rw,sync,no_root_squash,no_subtree_check)";
 echo $nfsvol $info >> /etc/exports
 exportfs -a
 systemctl restart nfs-server
 echo "Volume Criado"
 echo "Criação finalizada"
 
-#du.sh
+# du.sh
 echo 'echo "---' $nome '--"' >> du.sh
 echo "du -sh /mnt/nfsvol/lv01/"$nome >> du.sh
 echo "Finalizada a adicao no script du.sh"
